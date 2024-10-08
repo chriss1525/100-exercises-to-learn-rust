58f9b6403295eadce3b469b38a4a1c3e32476a4c
chriss1525 <okothchris15@gmail.com>
2024-10-09 00:28:52 +0300

    while loop to calculate a factorial

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,14 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut result: u32 = 1;
+    let mut current: u32 = n;
+
+    while current > 1 {
+        result *= current;
+        current -= 1;
+    }
+    result
 }
 
 #[cfg(test)]
